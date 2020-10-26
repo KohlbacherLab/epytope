c08b8e0097caea2bac4e922586b000096d774f92
antschum <schum.antonia@gmail.com>
2020-10-26 19:02:46 +0100

    extract factory metaclasses

--- Fred2/CleavagePrediction/__init__.py
+++ Fred2/CleavagePrediction/__init__.py
@@ -19,27 +19,57 @@ except ImportError:
     pass
 
 
-class CleavageSitePredictorFactory(object):
-    class __metaclass__(type):
-        def __init__(cls, name, bases, nmspc):
-            type.__init__(cls, name, bases, nmspc)
+class metaclassCleavageSite(type):
+    def __init__(cls, name, bases, nmspc):
+        type.__init__(cls, name, bases, nmspc)
 
-        def __call__(self, _predictor, *args, **kwargs):
-            """
-            If a third person wants to write a new Cleavage Site Predictor. One has to name the file fred_plugin and
-            inherit from ACleavagePrediction. That's it nothing more.
-            """
+    def __call__(self, _predictor, *args, **kwargs):
+        """
+        If a third person wants to write a new Cleavage Site Predictor. One has to name the file fred_plugin and
+        inherit from ACleavagePrediction. That's it nothing more.
+        """
 
-            version = str(kwargs["version"]).lower() if "version" in kwargs else None
-            try:
-                return ACleavageSitePrediction[str(_predictor).lower(), version](*args)
-            except KeyError as e:
-                if version is None:
-                    raise ValueError("Predictor %s is not known. Please verify that such an Predictor is "%_predictor +
-                                "supported by FRED2 and inherits ACleavageSitePrediction.")
-                else:
-                    raise ValueError("Predictor %s version %s is not known. Please verify that such an Predictor is "%(_predictor, version) +
-                                "supported by FRED2 and inherits ACleavageSitePrediction.")
+        version = str(kwargs["version"]).lower() if "version" in kwargs else None
+        try:
+            return ACleavageSitePrediction[str(_predictor).lower(), version](*args)
+        except KeyError as e:
+            if version is None:
+                raise ValueError(
+                    "Predictor %s is not known. Please verify that such an Predictor is " % _predictor +
+                    "supported by FRED2 and inherits ACleavageSitePrediction.")
+            else:
+                raise ValueError(
+                    "Predictor %s version %s is not known. Please verify that such an Predictor is " % (
+                        _predictor, version) +
+                    "supported by FRED2 and inherits ACleavageSitePrediction.")
+
+
+class metaclassCleavageFragment(type):
+    def __init__(cls, name, bases, nmspc):
+        type.__init__(cls, name, bases, nmspc)
+
+    def __call__(self, _predictor, *args, **kwargs):
+        """
+        If a third person wants to write a new Cleavage Fragment Predictor. One has to name the file fred_plugin and
+        inherit from CleavageFragmentPredictorFactory. That's it nothing more.
+        """
+
+        version = str(kwargs["version"]).lower() if "version" in kwargs else None
+        try:
+            return ACleavageFragmentPrediction[str(_predictor).lower(), version](*args)
+        except KeyError as e:
+            if version is None:
+                raise ValueError(
+                    "Predictor %s is not known. Please verify that such an Predictor is " % _predictor +
+                    "supported by FRED2 and inherits ACleavageFragmentPrediction.")
+            else:
+                raise ValueError(
+                    "Predictor %s version %s is not known. Please verify that such an Predictor is " % (
+                        _predictor, version) +
+                    "supported by FRED2 and inherits ACleavageFragmentPrediction.")
+
+
+class CleavageSitePredictorFactory(metaclass=metaclassCleavageSite):
 
     @staticmethod
     def available_methods():
@@ -51,27 +81,7 @@ class CleavageSitePredictorFactory(object):
         return {k: sorted(versions.keys()) for k, versions in ACleavageSitePrediction.registry.items()}
 
 
-class CleavageFragmentPredictorFactory(object):
-    class __metaclass__(type):
-        def __init__(cls, name, bases, nmspc):
-            type.__init__(cls, name, bases, nmspc)
-
-        def __call__(self, _predictor, *args, **kwargs):
-            """
-            If a third person wants to write a new Cleavage Fragment Predictor. One has to name the file fred_plugin and
-            inherit from CleavageFragmentPredictorFactory. That's it nothing more.
-            """
-
-            version = str(kwargs["version"]).lower() if "version" in kwargs else None
-            try:
-                return ACleavageFragmentPrediction[str(_predictor).lower(), version](*args)
-            except KeyError as e:
-                if version is None:
-                    raise ValueError("Predictor %s is not known. Please verify that such an Predictor is "%_predictor +
-                                "supported by FRED2 and inherits ACleavageFragmentPrediction.")
-                else:
-                    raise ValueError("Predictor %s version %s is not known. Please verify that such an Predictor is "%(_predictor, version) +
-                                "supported by FRED2 and inherits ACleavageFragmentPrediction.")
+class CleavageFragmentPredictorFactory(metaclass=metaclassCleavageFragment):
 
     @staticmethod
     def available_methods():
--- Fred2/EpitopePrediction/__init__.py
+++ Fred2/EpitopePrediction/__init__.py
@@ -20,27 +20,30 @@ except ImportError:
     pass
 
 
-class EpitopePredictorFactory(object):
-    class __metaclass__(type):
-        def __init__(cls, name, bases, nmspc):
-            type.__init__(cls, name, bases, nmspc)
+class metaclassEpitope(type):
+    def __init__(cls, name, bases, nmspc):
+        type.__init__(cls, name, bases, nmspc)
 
-        def __call__(self, _predictor, *args, **kwargs):
-            '''
-            If a third person wants to write a new Epitope Predictior. He/She has to name the file fred_plugin and
-            inherit from AEpitopePrediction. That's it nothing more.
-            '''
+    def __call__(self, _predictor, *args, **kwargs):
+        '''
+        If a third person wants to write a new Epitope Predictior. He/She has to name the file fred_plugin and
+        inherit from AEpitopePrediction. That's it nothing more.
+        '''
 
-            version = str(kwargs["version"]).lower() if "version" in kwargs else None
-            try:
-                return AEpitopePrediction[str(_predictor).lower(), version](*args)
-            except KeyError as e:
-                if version is None:
-                    raise ValueError("Predictor %s is not known. Please verify that such an Predictor is "%_predictor +
-                                "supported by FRED2 and inherits AEpitopePredictor.")
-                else:
-                    raise ValueError("Predictor %s version %s is not known. Please verify that such an Predictor is "%(_predictor, version) +
-                                "supported by FRED2 and inherits AEpitopePredictor.")
+        version = str(kwargs["version"]).lower() if "version" in kwargs else None
+        try:
+            return AEpitopePrediction[str(_predictor).lower(), version](*args)
+        except KeyError as e:
+            if version is None:
+                raise ValueError("Predictor %s is not known. Please verify that such an Predictor is " % _predictor +
+                                 "supported by FRED2 and inherits AEpitopePredictor.")
+            else:
+                raise ValueError("Predictor %s version %s is not known. Please verify that such an Predictor is " % (
+                _predictor, version) +
+                                 "supported by FRED2 and inherits AEpitopePredictor.")
+
+
+class EpitopePredictorFactory(metaclass=metaclassEpitope):
 
     @staticmethod
     def available_methods():
