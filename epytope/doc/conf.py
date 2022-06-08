cd0f326f90b7f1b48846dcf7fd8179d31e9475c5
Jonas Scheid <43858870+jonasscheid@users.noreply.github.com>
2022-06-08 14:45:34 +0200

    Update epytope/doc/conf.py
    Co-authored-by: Gisela Gabernet <gisela.gabernet@gmail.com>

--- epytope/doc/conf.py
+++ epytope/doc/conf.py
@@ -269,7 +269,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'eptyope', 'epytope Documentation',
+    (master_doc, 'epytope', 'epytope Documentation',
      [author], 1)
 ]
 
